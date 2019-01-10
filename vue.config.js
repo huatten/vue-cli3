df9e64da3f139923243950a150c1dc942ba3b972
jinhua <tengjinhua@yunjingkeji.net>
2019-01-10 15:59:47 +0800

    purgecss-webpack-plugin插件去除多余无效的css

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const path = require("path");
+// https://www.npmjs.com/package/purgecss-webpack-plugin
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+//If you need multiple paths use the npm package glob-all instead of glob
+const glob = require("glob-all");
+
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 const resolve = dir => path.join(__dirname, dir);
-//const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+
 module.exports = {
   chainWebpack: config => {
     //配置webpack目录别名alias
@@ -9,6 +15,21 @@ module.exports = {
       .set("assets", resolve("src/assets"))
       .set("components", resolve("src/components"))
       .set("view", resolve("src/view"));
+  },
+  configureWebpack: config => {
+    if (IS_PROD) {
+      const plugins = [];
+      plugins.push(
+        new PurgecssPlugin({
+          paths: glob.sync([
+            path.join(__dirname, "./src/index.html"),
+            path.join(__dirname, "./**/*.vue"),
+            path.join(__dirname, "./src/**/*.js")
+          ])
+        })
+      );
+      config.plugins = [...config.plugins, ...plugins];
+    }
   },
   css: {
     loaderOptions: {

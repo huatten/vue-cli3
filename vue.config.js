c9cf05da940feec4cec813079d119040258f617a
jinhua <tengjinhua@yunjingkeji.net>
2019-01-11 19:24:59 +0800

    webpack-bundle-analyzer打包分析

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,14 @@ const PurgecssPlugin = require("purgecss-webpack-plugin");
 const glob = require("glob-all"); //If you need multiple paths use the npm package glob-all instead of glob
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
-const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV); //是否生产环境
 const IS_CDN = false; //是否开启CDN引用
+const IS_GZIP = true; //是否开启Gzip压缩
 const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
   chainWebpack: config => {
-    //配置webpack目录别名alias
+    // 目录别名alias
     config.resolve.alias
       .set("@", resolve("src"))
       .set("assets", resolve("src/assets"))
@@ -24,13 +25,13 @@ module.exports = {
       .loader("image-webpack-loader")
       .options({
         bypassOnDebug: true
-        // mozjpeg: { progressive: true, quality: 65 },
-        // optipng: { enabled: false },
-        // pngquant: { quality: "65-90", speed: 4 },
-        // gifsicle: { interlaced: false },
-        // webp: { quality: 75 }
       })
       .end();
+
+    // 打包分析
+    config
+      .plugin("webpack-bundle-analyzer")
+      .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin);
   },
   configureWebpack: config => {
     //去除无效css
@@ -81,15 +82,15 @@ module.exports = {
     }
 
     //开启gzip
-    if (IS_PROD) {
+    if (IS_PROD && IS_GZIP) {
       const plugins = [];
       plugins.push(
         new CompressionWebpackPlugin({
           filename: "[path].gz[query]",
           algorithm: "gzip",
           test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
-          threshold: 10240, //只处理比这个值大的资源。按字节计算
-          minRatio: 0.8 //只有压缩率比这个值小的资源才会被处理
+          threshold: 10240,
+          minRatio: 0.8
         })
       );
       config.plugins = [...config.plugins, ...plugins];

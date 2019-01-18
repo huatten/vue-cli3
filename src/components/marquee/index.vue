fa22bfd30af2295a47cfed6a45ca0ecc32d692a5
jinhua <tengjinhua@yunjingkeji.net>
2019-01-18 15:40:21 +0800

    mock抽奖接口

--- src/components/marquee/index.vue
+++ src/components/marquee/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-marquee" >
     <ul class="m-marquee-cont" :class="{'m-marquee-top': animate}">
-      <li v-for="(item, index) in msgList" :key="index">
+      <li v-for="(item, index) in scrollList" :key="index">
         <span>{{item.phone}}</span>
         <span>{{item.name}}</span>
         <span>{{item.type}}</span>
@@ -15,50 +15,23 @@ export default {
   name: "m-marquee",
   data() {
     return {
-      animate: false,
-      msgList: [
-        {
-          phone: "178****9005",
-          name: "小藤藤",
-          type: "50元现金红包",
-          time: "1分钟前"
-        },
-        {
-          phone: "138****8556",
-          name: "miss鬼鬼",
-          type: "100元现金红包",
-          time: "2分钟前"
-        },
-        {
-          phone: "135****5200",
-          name: "萧敬腾",
-          type: "10g金条",
-          time: "3分钟前"
-        },
-        {
-          phone: "188****9888",
-          name: "马化腾",
-          type: "1000元现金红包",
-          time: "4分钟前"
-        },
-        {
-          phone: "139****9655",
-          name: "小藤藤",
-          type: "100g金条",
-          time: "5分钟前"
-        }
-      ]
+      animate: false
     };
   },
+  props: {
+    scrollList: {
+      type: Array
+    }
+  },
   mounted() {
-    this.msgList.length > 2 && setInterval(this.scroll, 2100);
+    setInterval(this.scroll, 2100);
   },
   methods: {
     scroll() {
       this.animate = true;
       setTimeout(() => {
-        this.msgList.push(this.msgList[0]);
-        this.msgList.shift();
+        this.scrollList.push(this.scrollList[0]);
+        this.scrollList.shift();
         this.animate = false;
       }, 500);
     }
--- src/views/mine/mine.vue
+++ src/views/mine/mine.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="lottery">
     <ul>
-      <li v-for="(item,index) in prizeInfo" :key="index" ref="pice" :class="amplification_index === item.level ? 'selected' : ''">
+      <li 
+       v-for="(item, index) in prizeInfo" 
+       :key="index" 
+       ref="pice" 
+       :class="amplification_index === item.level ? 'selected' : ''"
+       @click="startrolling(item)"
+      >
         <img :src="item.picUrlDesc" v-if="item.picUrlDesc" />
       </li>
     </ul>
-    <m-marquee></m-marquee>
+    <m-marquee :scrollList="scrollList"></m-marquee>
   </div>
   
 </template>
 
 <script type="text/ecmascript-6">
+import axios from "axios";
 import MMarquee from "components/marquee/index";
-/**
- * https://github.com/Venler/vue-lottery
- * https://github.com/jlianphoto/iver/blob/master/src/packages/lottery/src/lottery.vue
- * https://blog.csdn.net/YaoDeBiAn/article/details/82767831
- * https://github.com/moshanghan/vue-mo-cli
- * https://blog.csdn.net/hushilin001/article/details/78606618/
- * https://www.bbsmax.com/A/KE5QXMe0JL/
- */
 export default {
   name: "minePage",
   data() {
@@ -33,91 +32,76 @@ export default {
       finalindex: 0, //最终的奖励距离当前的位置！不是最后的几等奖！
       myInterval: "", //定时器
       max_speed: 40, //滚盘的最大速度
-      minturns: 6, //最小的圈数为2
+      minturns: 8, //最小的圈数为2
       runs_now: 0, //当前已跑步数
+      startStatus: "", //活动状态
+      prizeInfo: [], //奖项列表
       residue_num: 3, //剩余次数
       prizeLevel: "", //中奖等级
       prizeName: "", // 奖品名称
       prizeUrl: "", // 奖品图片
-      prizeInfo: [
-        {
-          level: 1,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/1-2.png",
-          prizeId: "436066c40529401287658bfd67c1d346",
-          commodityName: "华为 P10 Plus 全网通"
-        },
-        {
-          level: 2,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/2-2.png",
-          prizeId: "acdcb838bda74ec8b1fd202234f852ec",
-          commodityName: "200优惠劵"
-        },
-        {
-          level: 3,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/3-2.png",
-          prizeId: "484bf4c856b94265960b3e182e9f597f",
-          commodityName: "100优惠劵"
-        },
-        {
-          level: 8,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/4-2.png",
-          prizeId: "d5c7784c4c4d4a33b141fc1be3b11a71",
-          commodityName: "15福币"
-        },
-        {
-          level: 4,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/5-2.png",
-          prizeId: "7221846d585a4bed80bf486f94fcabae",
-          commodityName: "10福币"
-        },
-        {
-          level: 7,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/6-1.png",
-          prizeId: "33c6413801fd44c594cbf6642840a614",
-          commodityName: "8福币"
-        },
-        {
-          level: 6,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/7-1.png",
-          prizeId: "e453f94905334ea083fca649e87b3308",
-          commodityName: "5福币"
-        },
-        {
-          level: 5,
-          picUrlDesc: "http://qdtalk.com/wp-content/uploads/2017/09/8-1.png",
-          prizeId: "e8df88de1878428bb58d0cc9152d8849",
-          commodityName: "3福币"
-        }
-      ]
+      scrollList: [] //滚动中奖信息
     };
   },
   components: { MMarquee },
   mounted() {
-    this.prizeInfo.splice(4, 0, " ");
-    console.log(this.$refs.pice[4]);
-    this.startrolling();
+    this.getinfo();
+    this.scrollInfo();
   },
   methods: {
-    //开始滚动
-    startrolling: function() {
-      this.$refs.pice[4].onclick = () => {
-        let _this = this;
-        //初始化步数
-        _this.runs_now = 0;
-        //当前可以点击的状态下
-        if (_this.roll_flag) {
-          _this.roll_flag = false;
-          //启动滚盘，注，若是最终后台无返回就不好意思里
-          _this.rolling();
-        }
-      };
+    //获取奖品信息
+    getinfo() {
+      axios
+        .get(
+          "https://easy-mock.com/mock/5c4137f5cee47f2c67974e24/api/prizeInfo"
+        )
+        .then(res => {
+          const data = res.data;
+          if (data.code === 0) {
+            data.data.prizeInfo.splice(4, 0, " ");
+            this.prizeInfo = data.data.prizeInfo;
+          }
+        });
     },
-    //抽奖结果
-    result() {
-      setTimeout(() => {
-        this.finalindex = this.prizeInfo[0].level;
-        this.prizeName = this.prizeInfo[0].commodityName;
-      }, 2000);
+    //获取滚动信息
+    scrollInfo() {
+      axios
+        .get("https://easy-mock.com/mock/5c4137f5cee47f2c67974e24/api/winList")
+        .then(res => {
+          const data = res.data;
+          if (data.code === 0) {
+            this.scrollList = data.data.list;
+          }
+        });
+    },
+    //初始化状态
+    init() {
+      this.amplification_index = 0;
+      //初始化步数
+      this.runs_now = 0;
+    },
+    //开始滚动
+    startrolling(item) {
+      if (item.level) return;
+      //当前可以点击的状态下
+      if (this.roll_flag) {
+        this.roll_flag = false; //不能点击了
+        axios
+          .get(
+            "https://easy-mock.com/mock/5c4137f5cee47f2c67974e24/api/winInfo"
+          )
+          .then(res => {
+            const data = res.data;
+            if (data.code === 0) {
+              //启动滚盘
+              this.rolling();
+              this.finalindex = data.data.level;
+              this.prizeName = data.data.commodityName;
+            } else {
+              alert("服务端错误");
+            }
+          });
+      }
     },
     //滚动轮盘的动画效果
     rolling() {
@@ -127,10 +111,7 @@ export default {
       }, this.speed);
       this.runs_now++; //已经跑步数加一
       this.amplification_index++; //当前的加一
-      //获取总步数，接口延迟问题，所以最后还是设置成1s以上
-      setTimeout(() => {
-        this.result();
-      }, 0);
+      //获取总步数
       const count_num =
         this.minturns * this.max_number + this.finalindex - this.last_index;
       //上升期间
@@ -142,8 +123,8 @@ export default {
       }
       //抽奖结束
       else if (this.runs_now >= count_num) {
-        clearInterval(this.myInterval);
         this.roll_flag = true;
+        clearInterval(this.myInterval);
         this.showAlert();
       }
       //下降期间
@@ -167,6 +148,9 @@ export default {
       setTimeout(() => {
         alert(`恭喜你抽到了${this.prizeName}`);
       }, 200);
+      setTimeout(() => {
+        this.init();
+      }, 300);
     }
   }
 };

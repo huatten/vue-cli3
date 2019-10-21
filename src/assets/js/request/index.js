5450cc6699c769fde08b0c463952ff1a9e7be88d
MrTenger <tengjinhua@yunjingkeji.net>
2019-10-21 17:43:59 +0800

    :art: 干掉愚蠢的switch

--- src/assets/js/request/index.js
+++ src/assets/js/request/index.js
@@ -13,45 +13,15 @@ const redirectToNeterror = router => {
 };
 //错误消息处理
 const showStatus = (status) => {
-  let message = ''
-  switch (status) {
-    case 400:
-      message = '请求错误(400)'
-      break
-    case 401:
-      message = '未授权，请重新登录(401)'
-      break
-    case 403:
-      message = '拒绝访问(403)'
-      break
-    case 404:
-      message = '请求不存在(404)'
-      break
-    case 408:
-      message = '请求超时(408)'
-      break
-    case 500:
-      message = '服务器错误(500)'
-      break
-    case 501:
-      message = '服务未实现(501)'
-      break
-    case 502:
-      message = '网络错误(502)'
-      break
-    case 503:
-      message = '服务不可用(503)'
-      break
-    case 504:
-      message = '网络超时(504)'
-      break
-    case 505:
-      message = 'HTTP版本不受支持(505)'
-      break
-    default:
-      message = `网络连接错误(${status})`
+  const STATUS = [400, 401, 403, 404, 405, 408, 500, 501, 502, 503, 504, 505];
+  const MESSAGE = ['请求错误', '未授权，请重新登录', '拒绝访问', '请求不存在', '请求超时', '服务器错误', '服务未实现', '网络错误', '服务不可用', '网络超时', 'HTTP版本不受支持', '网络连接错误'];
+  for (let i = 0; i < STATUS.length; i++) {
+    if (status >= STATUS[i]) {
+      return `${MESSAGE[i]}(${status})`;
+    }
   }
-  return message;
+  //若不存在则返回最后一条默认消息
+  return `${MESSAGE[MESSAGE.length - 1]}(${status})`;
 };
 
 const request = option => {
